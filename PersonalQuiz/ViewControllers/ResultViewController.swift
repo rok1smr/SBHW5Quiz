5111f3f3505ef8d8087e6ed8c6d48f8c980ce744
ROK1SMR <runnerbkln@gmail.com>
2021-11-04 00:01:43 +0300

    Update

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -13,28 +13,18 @@ class ResultViewController: UIViewController {
     @IBOutlet weak var resultText: UILabel!
     
     var resultAnswers:[Answer] = []
-    
     let resultsOfQuiz = AnimalType.self
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: true);
 
-        print(resultAnswers.count)
-        let resultDog = resultAnswers.filter { $0.type == .dog }
-        let resultCat = resultAnswers.filter { $0.type == .cat }
-        let resultRabbit = resultAnswers.filter { $0.type == .rabbit }
-        let resultTurtle = resultAnswers.filter { $0.type == .turtle}
-        print("Dogs - \(resultDog.count), Cats - \(resultCat.count), Rabbits - \(resultRabbit.count), Turtles - \(resultTurtle.count) ")
-        print("\(resultsOfQuiz.turtle.definition)")
+        let resultDog = resultAnswers.filter { $0.type == .dog } .count
+        let resultCat = resultAnswers.filter { $0.type == .cat } .count
+        let resultRabbit = resultAnswers.filter { $0.type == .rabbit } .count
+        let resultTurtle = resultAnswers.filter { $0.type == .turtle} .count
+        print("Dogs - \(resultDog), Cats - \(resultCat), Rabbits - \(resultRabbit), Turtles - \(resultTurtle) ")
         
     }
-
-
-    // 1. Передать сюда массив с ответами
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результаты в соотвствии с этим животным
-    // 4. Избавиться от кнопки возврата назад на экране результатов
-    
 }
 
